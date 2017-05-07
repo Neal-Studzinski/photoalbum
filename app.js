81a8c59ae71a3bb5c4c9371e095e86d2277c20aa
NEAL STUDZINSKI <nealstudzinski@gmail.com>
2017-05-06 20:44:22 -0500

    completed

--- app.js
+++ app.js
@@ -1,17 +1,93 @@
-console.log(jQuery);
+function removeAllViews() {
+  $('.home-view').remove();
+  $('.album-view').remove();
+  $('.photo-view').remove();
+  $('body').css('background-color', 'white');
+}
 
-var $albums = $('.albums-page');  //Main page
-var $pageTwo = $('.photos-page'); //Individual Album page
-var $sidebar = $('.sidebar'); // Sidebar navigation
-var $albumCon = $('.album-container'); //Album layout
-var $pageThree = $('.pic-page'); //Individual picture page
-var $backBtn = $('.backBtn'); // Button to go backBtn
+function renderHome() {
+  removeAllViews();
 
+  //home-view container
+  $('body').append('<div class="home-view"></div>');
+  //heading My Photo Albums
+  $('.home-view').append('<h1>My Photo Albums</h1>');
+  //unordered list
+  $('.home-view').append('<ul></ul>');
+  //loop to put list items and onclick
+  albums.forEach(function(element, index, array){
+    $('ul').append('<li><a id="' + element['id'] + '" href="#"><img src="' + element['photos'][0]['url']+ '" alt="">' + element['name']+ '</a></li>');
+  });
+  $('.home-view a').on('click', renderAlbum);
+}
 
+function findAlbum(albumId) {
+  var selectedAlbum;
+  albums.forEach(function (album, index, array){
+    if ( album['id'] == albumId ) {
+      selectedAlbum = album;
+    }
+  })
+  return selectedAlbum;
+}
 
-// Started going through jquery on codeacademy
-$(document).ready(function() {
-    $('pagetwo').click(function() {
-        $('albums').removeClass('');
+function findPhoto(photoId) {
+  var selectedPhoto;
+  albums.forEach(function (album, index, array){
+    album['photos'].forEach(function(photo, index, array){
+      if ( photo['id'] == photoId ) {
+        selectedPhoto = photo;
+      }
     });
-});
+  });
+  return selectedPhoto;
+}
+
+function renderAlbum(clickEvent) {
+  removeAllViews();
+
+  var selectedAlbum = findAlbum(clickEvent.currentTarget.id);
+  //album-view container
+  $('body').append('<div class="album-view"></div>');
+  //insert album title
+  $('.album-view').append('<h2>' + selectedAlbum['name'] + '</h2>');
+  //left side navigation bar
+  $('.album-view').append('<nav class="album-nav"></nav>');
+  //unordered list
+  $('.album-nav').append('<ul></ul>');
+  //loop to build links in album-nav
+  albums.forEach(function(album, index, array){
+    if ( album['id'] === clickEvent.currentTarget.id ) {
+      $('.album-nav ul').append('<li id=""><a href="#" class="selected" id="' +  album['id'] + '">' + album['name']+ '</a></li>');
+    } else {
+      $('.album-nav ul').append('<li id=""><a href="#" id="' +  album['id'] + '">' + album['name']+ '</a></li>');
+    }
+  });
+  //adding listener to album nav links
+  $('.album-nav a').on('click', renderAlbum);
+  //album-contents container
+  $('.album-view').append('<div class="album-contents"></div>');
+  //insert unordered list into container
+  $('.album-contents').append('<ul></ul>');
+  //insert photos into album container
+  selectedAlbum['photos'].forEach(function(element, index, array){
+    $('.album-contents ul').append('<li><a id="' + element['id'] + '" href="#"><img src="' + element['url'] + '" alt="">' + element['name']+ '</a></li>');
+  });
+  $('.album-contents a').on('click', renderImage);
+}
+
+
+
+function renderImage(clickEvent) {
+  var selectedPhoto = findPhoto(clickEvent.currentTarget.id);
+  removeAllViews();
+
+  $('body').css('background-color', 'black');
+  $('body').append('<div class="photo-view"></div>');
+  $('.photo-view').append('<h2>' + selectedPhoto['name'] + '</h2>');
+  $('.photo-view').append('<a id="' + selectedPhoto['album'] + '" href="#"> Back to ' + selectedPhoto['album'] + '</a>');
+  $('.photo-view a').on('click', renderAlbum);
+  $('.photo-view').append('<img src="' + selectedPhoto['url'] + '" alt="">')
+}
+
+renderHome();
---
+++ data.js
@@ -0,0 +1,243 @@
+var albums = [
+  {
+    name: 'Album 1',
+    id: 'album1',
+    photos: [{
+       name: 'Photo 1',
+       url: 'images/animal1.jpg',
+       id: 'photo1',
+       album: 'album1'
+     },
+     {
+       name: 'Photo 2',
+       url: 'images/animal2.jpg',
+       id: 'photo2',
+       album: 'album1'
+     },
+     {
+       name: 'Photo 3',
+       url: 'images/animal3.jpg',
+       id: 'photo3',
+       album: 'album1'
+    },
+     {
+        name: 'Photo 4',
+        url: 'images/animal4.jpg',
+        id: 'photo4',
+        album: 'album1'
+    },
+    {
+        name: 'Photo 5',
+        url: 'images/animal5.jpg',
+        id: 'photo5',
+        album: 'album1'
+    },
+    {
+        name: 'Photo 6',
+        url: 'images/animal6.jpg',
+        id: 'photo6',
+        album: 'album1'
+    }]
+   },
+   {
+     name: 'Album 2',
+     id: 'album2',
+     photos: [{
+       name: 'Photo 1',
+       url: 'images/beach1.jpg',
+       id: 'photo1',
+       album: 'album2'
+     },
+     {
+       name: 'Photo 2',
+       url: 'images/beach2.jpg',
+       id: 'photo2',
+       album: 'album2'
+     },
+     {
+       name: 'Photo 3',
+       url: 'images/beach3.jpg',
+       id: 'photo6',
+       album: 'album2'
+   },
+   {
+       name: 'Photo 4',
+       url: 'images/beach4.jpg',
+       id: 'photo4',
+       album: 'album2'
+   },
+   {
+       name: 'Photo 5',
+       url: 'images/beach5.jpg',
+       id: 'photo5',
+       album: 'album2'
+   },
+   {
+       name: 'Photo 6',
+       url: 'images/beach6.jpg',
+       id: 'photo6',
+       album: 'album2'
+   }]
+   },
+  {
+    name: 'Album 3',
+    id: 'album3',
+    photos: [{
+      name: 'Photo 1',
+      url: 'images/forest1.jpg',
+      id: 'photo1',
+      album: 'album3'
+    },
+    {
+      name: 'Photo 2',
+      url: 'images/forest2.jpg',
+      id: 'photo2',
+      album: 'album3'
+    },
+    {
+      name: 'Photo 3',
+      url: 'images/forest3.jpg',
+      id: 'photo3',
+      album: 'album3'
+  },
+  {
+      name: 'Photo 4',
+      url: 'images/forest4.jpg',
+      id: 'photo4',
+      album: 'album3'
+  },
+  {
+      name: 'Photo 5',
+      url: 'images/forest5.jpg',
+      id: 'photo5',
+      album: 'album3'
+  },
+  {
+      name: 'Photo 6',
+      url: 'images/forest6.jpg',
+      id: 'photo6',
+      album: 'album3'
+  }]
+  },
+  {
+    name: 'Album 4',
+    id: 'album4',
+    photos: [{
+      name: 'Photo 1',
+      url: 'images/moon1.jpg',
+      id: 'photo1',
+      album: 'album4'
+    },
+    {
+      name: 'Photo 2',
+      url: 'images/moon2.jpg',
+      id: 'photo2',
+      album: 'album4'
+    },
+    {
+      name: 'Photo 3',
+      url: 'images/moon3.jpg',
+      id: 'photo3',
+      album: 'album4'
+  },
+  {
+      name: 'Photo 4',
+      url: 'images/moon4.jpg',
+      id: 'photo4',
+      album: 'album4'
+  },
+  {
+      name: 'Photo 5',
+      url: 'images/moon5.jpg',
+      id: 'photo5',
+      album: 'album4'
+  },
+  {
+      name: 'Photo 6',
+      url: 'images/moon6.jpg',
+      id: 'photo6',
+      album: 'album4'
+  }]
+  },
+  {
+    name: 'Album 5',
+    id: 'album5',
+    photos: [{
+      name: 'Photo 1',
+      url: 'images/sunset1.jpg',
+      id: 'photo1',
+      album: 'album5'
+    },
+    {
+      name: 'Photo 2',
+      url: 'images/sunset2.jpg',
+      id: 'photo2',
+      album: 'album5'
+    },
+    {
+      name: 'Photo 3',
+      url: 'images/sunset3.jpg',
+      id: 'photo3',
+      album: 'album5'
+  },
+  {
+      name: 'Photo 4',
+      url: 'images/sunset4.jpg',
+      id: 'photo4',
+      album: 'album5'
+  },
+  {
+      name: 'Photo 5',
+      url: 'images/sunset5.jpg',
+      id: 'photo5',
+      album: 'album5'
+  },
+  {
+      name: 'Photo 6',
+      url: 'images/sunset6.jpg',
+      id: 'photo6',
+      album: 'album5'
+  }]
+  },
+  {
+    name: 'Album 6',
+    id: 'album6',
+    photos:
+    [{
+      name: 'Photo 1',
+      url: 'images/tornado1.jpg',
+      id: 'photo1',
+      album: 'album6'
+    },
+    {
+      name: 'Photo 2',
+      url: 'images/tornado2.jpg',
+      id: 'photo2',
+      album: 'album6'
+    },
+    {
+      name: 'Photo 3',
+      url: 'images/tornado3.jpg',
+      id: 'photo3',
+      album: 'album6'
+  },
+  {
+      name: 'Photo 4',
+      url: 'images/tornado4.jpg',
+      id: 'photo4',
+      album: 'album6'
+  },
+  {
+      name: 'Photo 5',
+      url: 'images/tornado5.jpg',
+      id: 'photo5',
+      album: 'album6'
+  },
+  {
+      name: 'Photo 6',
+      url: 'images/tornado6.jpg',
+      id: 'photo6',
+      album: 'album6'
+  }]
+  }
+]
